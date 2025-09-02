d7093b80abcbd030b0fdc1fca6d96771cb36e201
Douglas Bullard <dbullard@nurflugel.com>
2025-09-01 17:28:50 -0700

    Cleaned up args and help

---
+++ issues.md
@@ -0,0 +1,55 @@
+##### **ShoTHR Issues list**
+
+- [ ] Skipping across the sand (i.e., dots)
+  - [ ] Rho skips
+  - [ ] Theta skips
+
+- [ ] All of the track isn't being drawn - skip?
+- [ ] Brighter sand
+- [ ] Make the image sized "fixed" - or at least keep it's aspect ratio when scaled up
+- [ ] Rename options and document nicely
+- [ ] Batch tracks
+  - [ ] Fix it so it doesn't have to be the last option
+  
+  - [ ]  - make a batch file version 
+    - [ ] Not just a list of tracks, add commands 
+      - [ ] At startup
+        - [ ] Table size
+        - [ ] Skip count
+        - [ ] Initial background
+  
+      - [ ] For each track
+        - [ ] Clean
+        - [ ] 2 Balls
+        - [ ] Reversed
+        - [ ] Skip Count
+  
+- [ ] Make animation option
+  - [ ] Takes the images that are drawn and saves them to a dir after displaying
+  - [ ] Optional - wrap FFMPEG and create the animation 
+    - [ ] Windows will suck
+
+- [ ] Show duration from previous status output
+
+
+
+##### **Long Term - Rendering farm**
+
+- [ ] Make a rendering farm of iMac & Raspberry Pi's?
+  - [ ] Need to coordinate which images go to which pi to avoid duplication.
+    - [ ] Make a web service for each pi which listens for image generation commands
+      - [ ] Draw Image
+      - [ ] Status of drawn image
+      - [ ] Fetch drawn image
+      - [ ] Delete image
+    - [ ] Main Mac will have a program that coordinates with the workers
+      - [ ] Can they broadcast availability to server at startup?
+        - [ ] Implies main server can listen to workers
+        - [ ] Don't fail if server isn't available - wait XXX and try again forever
+
+
+
+
+
+
+
--- src/main/kotlin/com/marginallyclever/showthr/Settings.kt
+++ src/main/kotlin/com/marginallyclever/showthr/Settings.kt
@@ -17,6 +17,7 @@ class Settings {
     var ballRadius: Int = 5
     var initialSandDepth: Double = 2.0
     var isGenerateCleanBackdrop = false
+    var cleanBeforeRendering = false
     var isReversed = false
     var shouldQuitWhenDone = false
     var backgroundImageName = ""
@@ -38,7 +39,7 @@ class Settings {
     var maxRadius = tableRadius - 20
     val deltaTime = 0.2
     val batchTracks: MutableList<String> = mutableListOf()
-
+    var batchTrackFile: String? = null
 
     /**
      * Read the command line arguments and set the inputFilename, outputFilename, w, h, ballSize, and initialDepth.
@@ -47,40 +48,19 @@ class Settings {
      * @return true if the arguments are valid
      */
     fun parseInputs(args: Array<String>): Boolean {
-        if (args.isEmpty()) {
-            println("Problem parsing arguments")
-            return false
-        }
+
         try {
-            var index = 0
-            while (index < args.size) {
-                when (args[index]) {
-                    "-b"             -> backgroundImageName = setValueFromArg(++index, args)
-                    "-useTwoBalls"   -> useTwoBalls = true
-                    "-c"             -> isGenerateCleanBackdrop = true
-                    "-d"             -> initialSandDepth = setValueFromArg(++index, args).toDouble()
-                    "-e"             -> shouldExpandSequences = setValueFromArg(++index, args).toBoolean()
-                    "-headless"      -> isHeadless = true
-                    "-i"             -> inputFilename = args[++index]
-                    "-g"             -> useGreyBackground = true
-                    "-skip"          -> imageSkipCount = setValueFromArg(++index, args).toInt()
-                    "-o"             -> outputFilename = setValueFromArg(++index, args)
-                    "-q"             -> shouldQuitWhenDone = true
-                    "-reversed"      -> isReversed = true
-                    "-s"             -> ballRadius = setValueFromArg(++index, args).toInt()
-                    "-tableDiameter" -> tableDiameter = setValueFromArg(++index, args).toInt()
-                    "-batchTracks"   -> batchTracks.addAll(setValueFromArg(++index, args).split(","))
-                    else             -> {
-                        println("Unknown option " + args[index])
-                        return false
-                    }
-                }
-                index++
-            }
+            getSettingsFromArgs(args)
         } catch (e: Exception) {
             println("Problem parsing arguments ${e.message}")
             return false
         }
+        adjustSettings()
+        calculateCenter()
+        return true
+    }
+
+    private fun adjustSettings() {
         if (!isHeadless && tableDiameter == 0) {
             tableDiameter = Toolkit.getDefaultToolkit().screenSize.height - 100
         }
@@ -104,11 +84,103 @@ class Settings {
             if (useTwoBalls) outputFilename = outputFilename!!.replace(".png", "_2balls.png")
             if (backgroundImageName.trim().isEmpty()) backgroundImageName = "clean_${tableDiameter}x${tableDiameter}.png"
         }
-        calculateCenter()
         // default output name to input name and png
         ext = outputFilename!!.substringAfterLast('.')
+    }
+
+    @Suppress("SpellCheckingInspection")
+    private fun getSettingsFromArgs(args: Array<String>): Boolean {
+        if (args.isEmpty()) {
+            println("Problem parsing arguments - no arguments")
+            printHelp()
+            return false
+        }
+        var index = 0
+        while (index < args.size) {
+            when (args[index].lowercase()) { // use lowercase, so if they get it wrong, it still works.
+                // mandatory arguments
+                "-i"              -> inputFilename = args[++index]
+                // Optional arguments with args and default values
+                "-background"     -> backgroundImageName = setValueFromArg(++index, args)
+                "-ballradius"     -> ballRadius = setValueFromArg(++index, args).toInt()
+                "-batchtrackfile" -> batchTrackFile = setValueFromArg(++index, args)
+                "-batchtracks"    -> batchTracks.addAll(setValueFromArg(++index, args).split(","))
+                "-depth"          -> initialSandDepth = setValueFromArg(++index, args).toDouble()
+                "-o"              -> outputFilename = setValueFromArg(++index, args)
+                "-skip"           -> imageSkipCount = setValueFromArg(++index, args).toInt()
+                "-tablesize"      -> tableDiameter = setValueFromArg(++index, args).toInt()
+
+                // Optional no-value arguments
+                "-2balls"         -> useTwoBalls = true
+                "-doclean"        -> cleanBeforeRendering = false
+                "-expandtracks"   -> shouldExpandSequences = setValueFromArg(++index, args).toBoolean()
+                "-gray"           -> useGreyBackground = true
+                "-headless"       -> isHeadless = true
+                "-makecleanbackdrop"              -> isGenerateCleanBackdrop = true
+                "-q"              -> shouldQuitWhenDone = true
+                "-reversed"       -> isReversed = true
+                else              -> {
+                    println("Unknown option " + args[index])
+                    printHelp()
+                    return false
+                }
+            }
+            index++
+        }
         return true
     }
+
+    // print the settings
+    fun printSettings() {
+        println("ShowTHR Settings")
+        println("   Mandatory arguments requiring values:")
+        println("       -i inputFilename = $inputFilename")
+
+        println("   Optional arguments requiring values:")
+        println("       -background = $backgroundImageName")
+        println("       -ballRadius = $ballRadius ")
+        println("       -batchTrackFile = $batchTrackFile")
+        println("       -batchTracks = $batchTracks")
+        println("       -depth - initialDepth = $initialSandDepth")
+        println("       -o - outputFilename = $outputFilename")
+        println("       -skip imageSkipCount = $imageSkipCount")
+        println("       -tableSize = $tableDiameter")
+        println("   Optional no-value arguments:")
+        println("       -2balls  = $useTwoBalls")
+        println("       -doClean  = $cleanBeforeRendering")
+        println("       -expandTracks  = $shouldExpandSequences")
+        println("       -gray = $useGreyBackground")
+        println("       -headless = $isHeadless ")
+        println("       -makeCleanBackdrop  = $isGenerateCleanBackdrop")
+        println("       -q  = $shouldQuitWhenDone")
+        println("       -reversed = $isReversed")
+    }
+
+    fun printHelp() {
+        println("ShowTHR Options")
+        println("   Mandatory arguments requiring values:")
+        println("       -i inputFilename ")
+
+        println("   Optional arguments requiring values:")
+        println("       -background background_image_name - use the image as the starting background")
+        println("       -ballRadius ball_radius - radius of the ball in pixels")
+        println("       -batchTrackFile file_name - read the tracks from a batch file like 'tracks.txt'.  These files can have have attributes like 2Balls, etc.")
+        println("       -batchTracks file1.thr,file2.thr... - A comma separated list of .thr files to process.  Overridden by -batchTrackFile")
+        println("       -depth - initialDepth - initial depth of the sand in pixels (only used if no background image is supplied)")
+        println("       -o - outputFilename - Optional - if not supplied, the inputFilename will be used with a .png extension")
+        println("       -skip imageSkipCount - number of track line images to skip while rendering (more makes the GUI preview play faster")
+        println("       -tableSize  - diameter of the table in pixels.  If not supplied, the screen size will be used")
+        println("   Optional no-value arguments:")
+        println("       -2balls - 2 balls can be better than 1")
+        println("       -doClean - If present, clean the sand before rendering")
+        println("       -expandTracks - Expand sequences of points to improve rendering quality")
+        println("       -gray - Use a grey background instead of the image background")
+        println("       -headless - No GUI, just render the image - this works on devices with no display")
+        println("       -makeCleanBackdrop - If present, make a backdrop image of clean sand for use in future renderings")
+        println("       -q - Quit after rendering the image")
+        println("       -reversed - Render the tracks in reverse order")
+    }
+
 
     fun calculateCenter() {
         tableRadius = tableDiameter / 2
@@ -126,20 +198,4 @@ class Settings {
         return true
     }
 
-    // print the settings
-    fun printSettings() {
-        println("inputFilename = $inputFilename")
-        println("b - backgroundImageName = $backgroundImageName")
-        println("useTwoBalls  = $useTwoBalls")
-        println("s - ballSize = $ballRadius")
-        println("tableRadius = $tableDiameter")
-        println("skip imageSkipCount = $imageSkipCount")
-        println("d - initialDepth = $initialSandDepth")
-        println("r - isReversed = $isReversed")
-        println("o - outputFilename = $outputFilename")
-        println("e - shouldExpandSequences = $shouldExpandSequences")
-        println("q - shouldQuitWhenDone = $shouldQuitWhenDone")
-        println("ext = $ext")
-
-    }
 }
