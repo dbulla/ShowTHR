8d7dd9aa71ba318fe3c1fcc0ecc0c8ed38a6cc8e
Douglas Bullard <dbullard@nurflugel.com>
2025-09-13 21:37:15 -0700

    Gradle fixes

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,10 @@
 plugins {
     // Apply the org.jetbrains.kotlin.jvm Plugin to add support for Kotlin.
-    alias(libs.plugins.kotlin.jvm)
-
+//    alias(libs.plugins.kotlin.jvm)
+    kotlin("jvm") version "2.1.20-Beta2"
     // Apply the application plugin to add support for building a CLI application in Java.
     application
-    id("com.gradleup.shadow") version "9.0.2"
+    id( "com.gradleup.shadow") version "9.1.0"
     id("com.github.ben-manes.versions") version "0.52.0"
     id("com.dorongold.task-tree") version "4.0.1"
 }
@@ -15,15 +15,15 @@ repositories {
 }
 
 dependencies {
-    testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
-    testImplementation(libs.junit.jupiter.engine)
-    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-
     implementation("javax.vecmath:vecmath:1.5.2")
     implementation("me.saharnooby:qoi-java:1.2.1")
     implementation("org.sejda.imageio:webp-imageio:0.1.6")
     implementation("com.google.guava:guava:33.4.8-jre")
     implementation("commons-io:commons-io:2.20.0")
+
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+    testImplementation("org.junit.jupiter:junit-jupiter-engine")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
 // Apply a specific Java toolchain to ease working on different environments.
@@ -35,7 +35,6 @@ java {
 
 application {
     mainClass = "com.marginallyclever.showthr.ShowTHR"
-//    mainClass = "com.nurflugel.showthr.Renamer"
 //    group = "com.marginallyclever"
 //    version = "0.0.1-SNAPSHOT"
 }
